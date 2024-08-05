9ae10c1e01be054cbc68be88ea64ceb7bb771e9e
Nariman Mamutov <nariman.mamutov@extrawest.com>
2024-08-05 22:47:48 +0300

    chore: Update dependencies, add xata integration

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,5 +1,15 @@
+"use client";
+
+import { useState } from "react";
+
 const ChatPage = () => {
-  return <div>ChatPage</div>;
+  const [fetching, setFetching] = useState(false);
+  return (
+    <div className="w-screen h-full flex flex-col bg-black text-wh text-white">
+      {/* list messages */}
+      {fetching && <div className="text-center font-bold"></div>}
+    </div>
+  );
 };
 
 export default ChatPage;
